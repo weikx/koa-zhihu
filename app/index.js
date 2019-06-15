37b982aaa0292c6b1397c39c100daa54f53adfc9
weikx <weikexin666@gmail.com>
2019-06-15 15:58:47 +0800

    增加错误处理

--- app/index.js
+++ app/index.js
@@ -2,7 +2,16 @@ const Koa = require('koa')
 const bodyparser = require('koa-bodyparser')
 const app = new Koa()
 const routing = require('./routes')
-
+app.use(async (ctx, next) => {
+  try {
+    await next()
+  } catch (err) {
+    ctx.state = err.status || err.statusCode || 500
+    ctx.body = {
+      message: err.message
+    }
+  }
+})
 app.use(bodyparser())
 routing(app)
 
