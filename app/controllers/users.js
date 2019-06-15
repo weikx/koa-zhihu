64c3a3ea89d84b7179651bc583f20e133238ceee
weikx <weikexin666@gmail.com>
2019-06-15 16:50:13 +0800

    增加 koa-parameter 校验参数

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -8,10 +8,18 @@ class UsersCtl {
     ctx.body = db[ctx.params.id * 1]
   }
   create (ctx) {
+    ctx.verifyParams({
+      name: {type: 'string'},
+      age: {type: 'number', required: false}
+    })
     db.push(ctx.request.body)
     ctx.body = ctx.request.body
   }
   update (ctx) {
+    ctx.verifyParams({
+      name: {type: 'string'},
+      age: {type: 'number', required: false}
+    })
     db[ctx.params.id * 1] = ctx.request.body
     ctx.body = ctx.request.body
   }
--- app/index.js
+++ app/index.js
@@ -3,11 +3,13 @@ const bodyparser = require('koa-bodyparser')
 const app = new Koa()
 const routing = require('./routes')
 const error = require('koa-json-error')
+const parameter = require('koa-parameter')
 
 app.use(error({
   postFormat: (err, {stack, ...rest}) => process.env.NODE_ENV === 'production' ? rest : {stack, rest}
 }))
 app.use(bodyparser())
+app.use(parameter(app))
 routing(app)
 
 app.listen(2333, () => console.log('app runing port 2333'))
