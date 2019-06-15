23efeab834dab83b9c80514f35e984fa0f260fdb
weikx <weikexin666@gmail.com>
2019-06-15 14:58:19 +0800

    init

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
 const Koa = require('koa')
+const bodyparser = require('koa-bodyparser')
 const app = new Koa()
 const Router = require('koa-router')
 const router = new Router()
+const usersRouter = new Router({prefix: '/users'})
 
-router.get('/', (cxt, next) => {
+const auth = async (ctx, next) => {
+  if (ctx.url !== '/users') {
+    ctx.throw(401)
+  }
+  await next()
+}
+
+router.get('/', auth, (cxt, next) => {
   cxt.body = '这是首页'
 })
 
-router.get('/users', (cxt, next) => {
+usersRouter.get('/', auth, (cxt, next) => {
   cxt.body = '这是users'
 })
 
-router.get('/users:id', (cxt, next) => {
-  cxt.body = `这是用户详情 ${ctx.params.id}`
+usersRouter.get('/:id', auth, (cxt, next) => {
+  cxt.body = `这是用户详情 ${cxt.params.id}`
 })
 
+usersRouter.post('/', auth, (ctx) => {
+  ctx.body = '创建一个用户'
+})
 
+usersRouter.post('/:shit', auth, (ctx) => {
+  ctx.body = `创建一个用户 ${ctx.params.shit}`
+})
+
+app.use(bodyparser())
 app.use(router.routes())
+app.use(usersRouter.routes())
+app.use(usersRouter.allowedMethods())
 
 app.listen(2333)
