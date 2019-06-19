7ae3910259862b2fd0629a04593365867a735531
weikx <weikexin666@gmail.com>
2019-06-19 13:08:53 +0800

    设计个人信息 schema

--- app/models/users.js
+++ app/models/users.js
@@ -3,7 +3,25 @@ const { Schema, model } = require('mongoose')
 const userSchema = new Schema({
   __v: { type: Number, select: false },
   name: { type: String, required: true },
-  password: { type: String, required: true, select: false }
+  password: { type: String, required: true, select: false },
+  avatar_url: { type: String },
+  gender: { type: String, enum: ['male', 'femail'], required: true },
+  headline: { type: String },
+  locations: { type: [{ type: String }] },
+  business: { type: String },
+  employment: {
+    type: [{
+      company: { type: String },
+      job: { type: String }
+    }]
+  },
+  education: {
+    school: { type: String },
+    mojor: { type: String },
+    deploma: { type: Number, enum: [1, 2, 3, 4, 5] },
+    entrance_year: { type: Number },
+    gurdation_year: { type: Number }
+  }
 })
 
 module.exports = model('User', userSchema)
